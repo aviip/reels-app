a37238fcef30e9d6602490b29b4784a9e8563744
Avi <avip3513@gmail.com>
2025-01-31 23:42:57 +0530

    Add database connection and typescript

---
+++ lib/db.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const MONGO_URI = process.env.MONGO_URI!;
+
+if (!MONGO_URI) {
+  throw new Error("Please define mongo URL");
+}
+
+let cached = global.mongoose!;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: true,
+      maxPoolSize: 10,
+    };
+
+    cached.promise = mongoose
+      .connect(MONGO_URI, opts)
+      .then(() => mongoose.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+
+  return cached.conn;
+}
+
+export default connectDB;
